5ab68af2ca5a234209f7568e5887c77220d8a313
danilaionutromica <danilaionut8@gmail.com>
2023-02-21 23:23:44 +0200

    TODO: See if it is working
    -

--- KNN.py
+++ KNN.py
@@ -1,5 +1,6 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import random 
 
 def euclidian_distance(point_A,point_B):
     dimension = len(point_A)
@@ -26,6 +27,36 @@ def vote(points,points_classes,point_to_clasify,k):
     count_no=0
     for index in indexes:
         if points_classes[index] == "Yes":
-            
+            count_Yes+=1
+        else:
+            count_no+=1
+    if count_Yes > count_no:
+        return "Yes"
+    else:
+        return "No"
+
+points=[]
+red_class=[]
+green_class=[]
+classes=[]
+for i in range(0,100):
+    x=random.randint(1,1000)
+    y=random.randint(1,1000)
+    if x>500:
+        classes.append("Yes")
+        green_class.append([x,y])
+    else:
+        classes.append("No")
+        red_class.append([x,y])
+    points.append([x,y])
+
+
+print(vote(points,classes,[720,100],50))
+plt.plot(red_class,'o', color='red')
+plt.plot(green_class,'o', color='green')
+plt.plot([720,100],'o', color='blue')
+plt.show()
+
+
         
         
