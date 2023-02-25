83bcee7846e5746e06c393a852a8dbeb9c1454da
danilaionutromica <danilaionut8@gmail.com>
2023-02-25 13:59:13 +0200

    Fix and updated TODO: Documentation and add comments
    Fix and updated TODO: Documentation and add comments

--- KNN.py
+++ KNN.py
@@ -1,62 +1,90 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random 
+import math
 
-def euclidian_distance(point_A,point_B):
-    dimension = len(point_A)
-    if dimension != len(point_B):
-        return None
-    sum=0
-    for i in range(0,dimension):
-        sum+= (point_A[i]-point_B[i]) **2
-    return np.sqrt(sum)
-    
-def get_max_val_index_excluded(values):
-    return values.index(max(values))
-
-def vote(points,points_classes,point_to_clasify,k):
-    distance_list=[]
-    for point in points:
-        distance_list.append(euclidian_distance(point,point_to_clasify))
-    indexes=[]
-    for i in range(0,k):
-        index=get_max_val_index_excluded(distance_list)
-        distance_list.remove(distance_list[index])
-        indexes.append(index)
-    count_Yes=0
-    count_no=0
-    for index in indexes:
-        if points_classes[index] == "Yes":
-            count_Yes+=1
+class KNN:
+    def __init__(self,K,list_of_points,points_classes,classes_names,point_to_clasifiy,distance="euclidian",classes_color=None):
+        self.k=K
+        self.points=list_of_points
+        self.distance=distance
+        self.point_to_clasifiy=point_to_clasifiy
+        self.points_classes=points_classes
+        self.classes_names=classes_names
+        self.close_points=[]
+        self.classes_color=classes_color
+    def calculate_distance(self,point_A,point_B,distance="euclidian"):
+        dimension = len(point_A)
+        if dimension != len(point_B):
+            return None
+        if distance == "euclidian":
+            sum=0
+            for i in range(0,dimension):
+                sum+= (point_A[i]-point_B[i])**2
+            return np.sqrt(sum)
+        if distance.startswith("L"):
+            distance=distance.replace("L","")
+            pr=float(distance)
+            sum=0
+            for i in range(0,dimension):
+                sum+=math.pow( np.abs(point_A[i] - point_B[i]),pr)
+            return sum
+    def get_max_val_index_excluded(self,values,excluded):
+        if excluded == []: 
+            return values.index(min(values))
         else:
-            count_no+=1
-    if count_Yes > count_no:
-        return "Yes"
-    else:
-        return "No"
+            index_min=0
+            minm=values[0]
+            for i in range(0,len(values)):
+                if values[i]<minm and i not in excluded:
+                    minm=values[i]
+                    index_min=i
+            return index_min
+    def vote(self):
+        distance_list=[]
+        for point in self.points:
+            distance_list.append(self.calculate_distance(point,self.point_to_clasifiy,self.distance))
+        indexes=[]
+        excluded_indexs=[]
+        for i in range(0,self.k):
+            index=self.get_max_val_index_excluded(distance_list,excluded_indexs)
+            self.close_points.append(points[index])
+            excluded_indexs.append(index)
+            indexes.append(index)
+        my_classes_counts={}
+        for elem in self.classes_names:
+            my_classes_counts[elem]= 0
+        for index in indexes:
+            for elem in my_classes_counts:
+                if self.points_classes[index] == elem:
+                    my_classes_counts[elem]+=1
+        return max(my_classes_counts, key=my_classes_counts.get)
 
+    def show_plt(self):
+        dimension=len(self.point_to_clasifiy)
+        for i in range(0,len(self.points)):
+            plt.plot(self.points[i][0],self.points[i][1],'o',color=self.classes_color[self.points_classes[i]])
+        for i in range(0,len(self.close_points)):
+            plt.plot(self.close_points[i][0],self.close_points[i][1],'o',color="black")
+        plt.plot(self.point_to_clasifiy[0],self.point_to_clasifiy[1],'o',color="blue")
+        plt.show()
+                    
+            
 points=[]
-red_class=[]
-green_class=[]
 classes=[]
-for i in range(0,100):
-    x=random.randint(1,1000)
-    y=random.randint(1,1000)
-    if x>500:
+
+for i in range(0,5000):
+    x=random.randint(0,100)
+    y=random.randint(0,100)
+    if x>50:
         classes.append("Yes")
-        green_class.append([x,y])
     else:
         classes.append("No")
-        red_class.append([x,y])
     points.append([x,y])
 
-
-print(vote(points,classes,[720,100],50))
-plt.plot(red_class,'o', color='red')
-plt.plot(green_class,'o', color='green')
-plt.plot([720,100],'o', color='blue')
-plt.show()
-
+knn=KNN(500,points,classes,["Yes","No"],[50,50],classes_color={"Yes" : "green" ,"No" : "red"})
+print(knn.vote())
+knn.show_plt()
 
         
         
